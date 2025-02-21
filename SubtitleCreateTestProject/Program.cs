d05fab7b3ae2ad1dc56ae33173f185728b8b06f7
kri_YamiHikari <kriYamiHikari@outlook.com>
2025-02-21 13:18:37 +0800

    build: install unocss, update photino config

--- SubtitleCreateTestProject/Program.cs
+++ SubtitleCreateTestProject/Program.cs
@@ -8,14 +8,20 @@ internal class Program
     private static void Main(string[] args)
     {
         var appBuilder = PhotinoBlazorAppBuilder.CreateDefault(args);
-
+        
         appBuilder.RootComponents.Add<App>("#app");
         appBuilder.Services.AddMasaBlazor();
 
         var app = appBuilder.Build();
 
         app.MainWindow
-            .SetTitle("Photino Blazor Sample");
+            .SetLogVerbosity(0)
+            .SetUseOsDefaultSize(false)
+            .Center()
+            .SetMinSize(620,550)
+            .SetSize(1280,720)
+            .SetFileSystemAccessEnabled(true)
+            .SetTitle("Nagare Subtitle Blazor test");
 
         AppDomain.CurrentDomain.UnhandledException += (sender, error) => { };
 
